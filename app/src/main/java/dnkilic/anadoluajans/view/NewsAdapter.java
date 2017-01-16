960b6a85de48becc7ba15bc7cf763e975c34829c
Ali <alid@mirasis.com.tr>
2017-01-17 01:29:24 +0300

    Fazlalık kütüphaneler temizlendi

--- app/src/main/java/dnkilic/anadoluajans/view/NewsAdapter.java
+++ app/src/main/java/dnkilic/anadoluajans/view/NewsAdapter.java
@@ -2,26 +2,19 @@ package dnkilic.anadoluajans.view;
 
 import android.content.Context;
 import android.content.Intent;
-import android.icu.text.TimeZoneFormat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
+
+
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.squareup.picasso.Picasso;
 
-import java.security.Timestamp;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Locale;
 
 import dnkilic.anadoluajans.DetailActivity;
 import dnkilic.anadoluajans.MainActivity;
